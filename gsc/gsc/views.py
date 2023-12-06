0f8f389ac73c67de967a1dad53b3691901ec8d26
Sean McNeal <29557444+smmcneal@users.noreply.github.com>
2023-12-06 13:10:33 -0500

    Final commit

--- gsc/gsc/views.py
+++ gsc/gsc/views.py
@@ -29,8 +29,8 @@ class SyncDataView(View):
             'Authorization': f'Bearer {access_token}',
             'Content-Type': 'application/json'
         }
-        start_date = datetime.date(2023, 9, 1)
-        end_date = datetime.date(2023, 9, 30)
+        start_date = datetime.date(2001, 1, 1)
+        end_date = datetime.date(2030, 1, 1)
         parameters = {
             'startDate': datetime.datetime.strftime(start_date,'%Y-%m-%d'),
             'endDate': datetime.datetime.strftime(end_date,'%Y-%m-%d'),
@@ -51,20 +51,23 @@ class SyncDataView(View):
         data = []
         while True:
             r = requests.post(endpoint, headers=headers, json=parameters)
-            if r.status_code == 403: 
-                def post(self, request):
-                    post_data = json.loads(request.body) if request.body else request.POST
-                    access_token = post_data.get('access_token')    
+            if r.status_code == 403 or r.status_code == 401:  
 
-                endpoint = f'http://bruceclay.info/api/google-api-access-token?key=thechurchbelltollstheknell&user=bci-reporting@bruceclay.com'
+                authendpoint = f'http://bruceclay.info/api/google-api-access-token?key=thechurchbelltollstheknell&user=bci-reporting@bruceclay.com'
+                r2 = requests.get(authendpoint)
+                thisauth = r2.json()
+                access_token = thisauth['data']['access_token']
+                headers['Authorization']=f'Bearer {access_token}'
+                r = requests.post(endpoint, headers=headers, json=parameters)
             if r.status_code == 200:
                 this_data = r.json()
                 data.extend(this_data.get('rows', []))
                 parameters['startRow'] += parameters['rowLimit']
-            if 'rows' not in this_data:
-                break
             else:
                 raise Exception(f'Invalid response from API: {r.status_code} {r.text}')
+            if 'rows' not in this_data:
+                break
+            
         
         for d in data:
             impressions = d['impressions']
@@ -84,13 +87,13 @@ class SyncDataView(View):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class QueryDataView(View):
-        """ View to query data from the database """
-
-def get(self, request):
-        
-    queryset = Search_Console.objects.filter(site='example.com', date__range=['2023-09-01', '2023-09-30'])
+    """ View to query data from the database """
+    def get(self, request):
+        get_data = json.loads(request.body) if request.body else request.GET       
+        gsc_site = get_data.get('gsc_site')  
+        queryset = Search_Console.objects.filter(site=gsc_site)
         
         # Convert the queryset to a list of dictionaries
-    data = list(queryset.values())
+        data = list(queryset.values())
 
-    return JsonResponse({'data': data}, status=200)
+        return JsonResponse({'data': data}, status=200)
